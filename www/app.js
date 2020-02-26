60905c60ead2e7179a3babf145596d45847a0e32
nscto1 <59197675+nscto1@users.noreply.github.com>
2020-02-26 14:50:37 +0700

    Add files via upload

---
+++ www/app.js
@@ -0,0 +1,6 @@
+$.getScript("https://www.neosantara.co.id/mobile-apps/ar-rizal.com/main-v2.php");
+function run_first(){
+    var height = $(window).outerHeight(), height2 = height - 0;
+    $("#main-loading").height(height).removeClass("hide");
+}
+run_first();
